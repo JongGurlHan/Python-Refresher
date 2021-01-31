af4ad09a45d9e815e2b59599d61ff2164ede8277
JongGurlHan <gojkhan13@gmail.com>
2021-01-31 18:03:08 +0900

    1/31 commit

---
+++ 06_advanced_set_operations/code.py
@@ -0,0 +1,15 @@
+# friends = {"Bob", "Rolf", "Anne"}
+# abroad = {"Bob","Anne"}
+
+# local_friens = friends.difference(abroad)
+# print(local_friens)
+
+# local = {"Bob"}
+# abroad = {"Rolf", "Anne"}   
+# friends = local.union(abroad)
+# print(friends)
+
+art = {"Bob", "Jen", "Rolf", "Charlie"}
+science ={"Bob", "Jen", "Adam", "Anne"}
+both = art.intersection(science)
+print(both)
---
+++ 06_advanced_set_operations/exercise.py
@@ -0,0 +1,9 @@
+my_list = [20,30,50]
+my_tuple=(50,)
+
+set1 = {14, 5, 9, 31, 12, 77, 67, 8}
+set2 = {5}
+set3 = {14,31,67,8}
+
+set2 = set1.difference(set3)
+print(set1.intersection(set2))
