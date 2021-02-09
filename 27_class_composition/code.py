767ff68ba68445c09260c59b1b0364d31a8e8fe2
JongGurlHan <gojkhan13@gmail.com>
2021-02-09 19:03:19 +0900

    02.09 commit

---
+++ 27_class_composition/code.py
@@ -0,0 +1,29 @@
+#composition allows your classes to be simpler and reduces teh complexity of your code overall 
+
+class BookShelf:
+    # def __init__(self, quantity):
+    #     self.quantity = quantity
+
+    def __str__(self):
+        return f"BookShelf with {self.quantity} books."
+
+shelf = BookShelf(300)
+
+class Book(BookShelf):
+    def __init__(self, name, quantity):
+        super().__init__(quantity)
+        self.name = name
+    
+    def __str__(self):
+        return f"Book {self.name}"
+
+book  = Book("Harry Potter", 120)
+print(book)
+
+#This is a bad appraoch for 2 reasons
+#1. conceptual reason: when you do inheritance, you're essentially treating like evolutionary inheritance
+#You're saying that a book is a bookshelf and sth more. 
+#2. Technical reason: You've got this book class that inherits from bookshelf, 
+# but actually you are not using inside it anything about the bookshelf. So you are completely overriding the str method
+
+#Instead of setting the quantity of books in the bookself, we're actually going to allow constructor to take in a number of books
